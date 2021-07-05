ca4a4b5769d7505d902a94e82488a1431ae65b34
414706657@QQ.com <hu19950206>
2021-07-05 17:20:31 +0800

    springCloudFeign

--- spring-cloud-service-provider/src/main/java/com/example/spring/cloud/consumer/controller/ServiceController.java
+++ spring-cloud-service-provider/src/main/java/com/example/spring/cloud/consumer/controller/ServiceController.java
@@ -1,6 +1,8 @@
 package com.example.spring.cloud.consumer.controller;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,11 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ServiceController {
 
+    private Logger logger = LoggerFactory.getLogger(ServiceController.class);
+
     @Value("${server.port}")
     private String port;
 
     @RequestMapping(value = "/hi", method = RequestMethod.GET)
     public String provider(@RequestParam(value = "message") String message) {
+        logger.info("message: "+message);
         return String.format("Hi，your message is : %s i am from port : %s", message, port);
     }
 }
